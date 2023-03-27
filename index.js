701e20df9ec7e2a4f8dc27196bf4b1f5cf7debeb
Michael Plumlee <michaelplumlee95@gmail.com>
2023-03-27 09:32:02 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+function findMatching(driverNames, search) {
+  search = search.toLowerCase();
+
+  let matchingDrivers = driverNames.filter(function (driver) {
+    driver = driver.toLowerCase();
+    return driver.includes(search);
+  });
+  return matchingDrivers;
+}
+
+function fuzzyMatch(driverNames, search) {
+  search = search.toLowerCase();
+
+  let matchingDrivers = driverNames.filter(function (driver) {
+    driver = driver.toLowerCase();
+    return driver.startsWith(search);
+  });
+  return matchingDrivers;
+}
+
+function matchName(drivers, search) {
+  let matchingDrivers = drivers.filter(function (drivers) {
+    return drivers.name === search;
+  });
+  return matchingDrivers;
+}
